1dcba4937cf72a8f0a2de2d55ffeecec60531f14
Gokulkrishna-Raju <gokulkrishna.raju@surveysparrrow.com>
2024-11-11 17:20:59 +0530

    iOS Beta Version Updated

--- ios/SsSurveyModule.swift
+++ ios/SsSurveyModule.swift
@@ -10,100 +10,102 @@ import Foundation
 import SurveySparrowSdk
 
 @objc(SsSurveyModule)
+@available(iOS 13.0, *)
 class SsSurveyModule:RCTEventEmitter,SsSurveyDelegate{
-  @IBOutlet weak var ssSurveyView: SsSurveyView!
-  public var surveyType: SurveySparrow.SurveyType = .CLASSIC
-  
-  @objc
-  func openSurvey(_ options: NSDictionary){
-    let domain = options.value(forKey: "domain") as! String
-    let token = options.value(forKey: "token") as! String
-    let queryItem = options.value(forKey: "customParams") as! [NSDictionary]
-    let surveyTypeParam = options.value(forKey: "surveyType") as! NSString
-    let thankYouRedirectTime = (options.value(forKey: "thankYouPageTimeLimit") as! NSString).doubleValue
-    var paramsmap: [String: String] = [:]
     
-    for val in queryItem {
-        let querName = val.value(forKey: "name") as! String
-        let querValue = val.value(forKey: "value") as! String
-        paramsmap[querName] = querValue
-    }
+    @IBOutlet weak var ssSurveyView: SsSurveyView!
+    public var surveyType: SurveySparrow.SurveyType = .CLASSIC
     
-    if(surveyTypeParam == "nps"){
-        surveyType = SurveySparrow.SurveyType.NPS
-    }
-    else{
-        surveyType = SurveySparrow.SurveyType.CLASSIC
-    }
-    
-    DispatchQueue.main.async {
-      let controller = RCTPresentedViewController();
-
-      let ssSurveyViewController = SsSurveyViewController()
-          ssSurveyViewController.domain = domain
-          ssSurveyViewController.token = token
-          ssSurveyViewController.surveyDelegate = self
-          ssSurveyViewController.params = paramsmap
-        ssSurveyViewController.thankyouTimeout = thankYouRedirectTime
-        ssSurveyViewController.surveyType = self.surveyType
+    @objc
+    func openSurvey(_ options: NSDictionary){
+        let domain = options.value(forKey: "domain") as! String
+        let token = options.value(forKey: "token") as! String
+        let queryItem = options.value(forKey: "customParams") as! [NSDictionary]
+        let surveyTypeParam = options.value(forKey: "surveyType") as! NSString
+        let thankYouRedirectTime = (options.value(forKey: "thankYouPageTimeLimit") as! NSString).doubleValue
+        var paramsmap: [String: String] = [:]
         
-      controller?.present(ssSurveyViewController, animated: true, completion: nil)
-    }
-    
-  }
-  
-  @objc
-  func scheduleSsSurvey(_ options: NSDictionary){
-    let domain = options.value(forKey: "domain") as! String
-    let token = options.value(forKey: "token") as! String
-    let queryItem = options.value(forKey: "customParams") as! [NSDictionary]
-    let surveyTypeParam = options.value(forKey: "surveyType") as! NSString
-    let thankYouRedirectTime = (options.value(forKey: "thankYouPageTimeLimit") as! NSString).doubleValue
-    let alertTitle = options.value(forKey: "alertTitle") as! String
-    let alertMessage = options.value(forKey: "alertMessage") as! String
-    let alertPositiveMessage = options.value(forKey: "alertPositiveText") as! String
-    let alertNegativeMessage = options.value(forKey: "alertNegativeText") as! String
-    let startAfter = options.value(forKey: "startAfter") as! Int64
-    let repeatInterval = options.value(forKey: "repeatInterval") as! Int64
-    let repeatSurvey = options.value(forKey: "repeatSurvey") as! Bool
-    let incrementalSurvey = options.value(forKey: "incrementalRepeat") as! Bool
-    var paramsmap: [String: String] = [:]
-    
-    for val in queryItem {
-        let querName = val.value(forKey: "name") as! String
-        let querValue = val.value(forKey: "value") as! String
-        paramsmap[querName] = querValue
-    }
-    
-    if(surveyTypeParam == "nps"){
-        surveyType = SurveySparrow.SurveyType.NPS
-    }
-    else{
-        surveyType = SurveySparrow.SurveyType.CLASSIC
-    }
-    
-    DispatchQueue.main.async {
-        let surveySparrow = SurveySparrow(domain: domain, token: token)
-        surveySparrow.params = paramsmap
-        surveySparrow.surveyType = self.surveyType
-        surveySparrow.thankyouTimout = thankYouRedirectTime
-        surveySparrow.alertTitle = alertTitle
-        surveySparrow.alertMessage = alertMessage
-        surveySparrow.alertPositiveButton = alertPositiveMessage
-        surveySparrow.alertNegativeButton = alertNegativeMessage
-        surveySparrow.startAfter = startAfter
-        surveySparrow.repeatInterval = repeatInterval
-        surveySparrow.repeatSurvey = repeatSurvey
-        surveySparrow.incrementalRepeat = incrementalSurvey
-        surveySparrow.surveyDelegate = self
+        for val in queryItem {
+            let querName = val.value(forKey: "name") as! String
+            let querValue = val.value(forKey: "value") as! String
+            paramsmap[querName] = querValue
+        }
         
-        let controller = RCTPresentedViewController();
-        surveySparrow.scheduleSurvey(parent: controller!)
+        if(surveyTypeParam == "nps"){
+            surveyType = SurveySparrow.SurveyType.NPS
+        }
+        else{
+            surveyType = SurveySparrow.SurveyType.CLASSIC
+        }
+        
+        DispatchQueue.main.async {
+            let controller = RCTPresentedViewController();
+            
+            let ssSurveyViewController = SsSurveyViewController()
+            ssSurveyViewController.domain = domain
+            ssSurveyViewController.token = token
+            ssSurveyViewController.surveyDelegate = self
+            ssSurveyViewController.params = paramsmap
+            ssSurveyViewController.thankyouTimeout = thankYouRedirectTime
+            ssSurveyViewController.surveyType = self.surveyType
+            
+            controller?.present(ssSurveyViewController, animated: true, completion: nil)
+        }
+        
     }
-  }
     
-  @objc
-  func clearscheduleSsSurvey(_ options: NSDictionary){
+    @objc
+    func scheduleSsSurvey(_ options: NSDictionary){
+        let domain = options.value(forKey: "domain") as! String
+        let token = options.value(forKey: "token") as! String
+        let queryItem = options.value(forKey: "customParams") as! [NSDictionary]
+        let surveyTypeParam = options.value(forKey: "surveyType") as! NSString
+        let thankYouRedirectTime = (options.value(forKey: "thankYouPageTimeLimit") as! NSString).doubleValue
+        let alertTitle = options.value(forKey: "alertTitle") as! String
+        let alertMessage = options.value(forKey: "alertMessage") as! String
+        let alertPositiveMessage = options.value(forKey: "alertPositiveText") as! String
+        let alertNegativeMessage = options.value(forKey: "alertNegativeText") as! String
+        let startAfter = options.value(forKey: "startAfter") as! Int64
+        let repeatInterval = options.value(forKey: "repeatInterval") as! Int64
+        let repeatSurvey = options.value(forKey: "repeatSurvey") as! Bool
+        let incrementalSurvey = options.value(forKey: "incrementalRepeat") as! Bool
+        var paramsmap: [String: String] = [:]
+        
+        for val in queryItem {
+            let querName = val.value(forKey: "name") as! String
+            let querValue = val.value(forKey: "value") as! String
+            paramsmap[querName] = querValue
+        }
+        
+        if(surveyTypeParam == "nps"){
+            surveyType = SurveySparrow.SurveyType.NPS
+        }
+        else{
+            surveyType = SurveySparrow.SurveyType.CLASSIC
+        }
+        
+        DispatchQueue.main.async {
+            let surveySparrow = SurveySparrow(domain: domain, token: token)
+            surveySparrow.params = paramsmap
+            surveySparrow.surveyType = self.surveyType
+            surveySparrow.thankyouTimout = thankYouRedirectTime
+            surveySparrow.alertTitle = alertTitle
+            surveySparrow.alertMessage = alertMessage
+            surveySparrow.alertPositiveButton = alertPositiveMessage
+            surveySparrow.alertNegativeButton = alertNegativeMessage
+            surveySparrow.startAfter = startAfter
+            surveySparrow.repeatInterval = repeatInterval
+            surveySparrow.repeatSurvey = repeatSurvey
+            surveySparrow.incrementalRepeat = incrementalSurvey
+            surveySparrow.surveyDelegate = self
+            
+            let controller = RCTPresentedViewController();
+            surveySparrow.scheduleSurvey(parent: controller!)
+        }
+    }
+    
+    @objc
+    func clearscheduleSsSurvey(_ options: NSDictionary){
         let domain = options.value(forKey: "domain") as! String
         let token = options.value(forKey: "token") as! String
         DispatchQueue.main.async {
@@ -112,24 +114,28 @@ class SsSurveyModule:RCTEventEmitter,SsSurveyDelegate{
         }
     }
     
-  override func supportedEvents() -> [String]! {
-    return["onSurveyResponse"]
-  }
-  
-  func handleSurveyResponse(response: [String : AnyObject]) {
-      sendEvent(withName: "onSurveyResponse", body: response)
-  }
+    override func supportedEvents() -> [String]! {
+        return["onSurveyResponse"]
+    }
     
-  func handleSurveyLoaded(response: [String : AnyObject]) {
+    func handleSurveyResponse(response: [String : AnyObject]) {
+        sendEvent(withName: "onSurveyResponse", body: response)
+    }
+    
+    func handleSurveyLoaded(response: [String : AnyObject]) {
         print(response)
-  }
+    }
     
     func handleSurveyValidation(response: [String : AnyObject]) {
-      print(response)
+        print(response)
     }
-  
-  override static func requiresMainQueueSetup() -> Bool {
-      return true
-  }
+    
+    func handleCloseButtonTap() {
+        print("Handle CloseButtonTap")
+    }
+    
+    override static func requiresMainQueueSetup() -> Bool {
+        return true
+    }
 }
 
