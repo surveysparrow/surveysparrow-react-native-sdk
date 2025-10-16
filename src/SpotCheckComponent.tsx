ab49683b70cb790bd032197ab19718e4a25db4aa
K Kalaiselvan <kalaiselvan.k@surveysparrow.com>
2025-10-16 13:19:56 +0530

    sdk-test

--- src/SpotCheckComponent.tsx
+++ src/SpotCheckComponent.tsx
@@ -409,8 +409,8 @@ const WebViewComponents: React.FC<WebViewComponentProps> = ({
       dispatch(
         updateState(
           webviewType === 'classic'
-            ? { classicWebViewRef: webViewRef }
-            : { chatWebViewRef: webViewRef }
+            ? { classicWebViewRef: webViewRef.current }
+            : { chatWebViewRef: webViewRef.current }
         )
       );
       console.log('Webview ref updated');
